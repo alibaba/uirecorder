195575fb4268ea26092e3ba850f3471a332d82c7
ali-lion <lion.wl@alibaba-inc.com>
2020-12-02 13:33:51 +0800

    add table for jinle support

--- template/javaTemplate.java
+++ template/javaTemplate.java
@@ -1,13 +1,27 @@
 package com.aliyun.iotx.test;
 
+import com.aliyun.iotx.framework.Operation;
+import com.aliyun.iotx.framework.testInit.BaseTest;
+import com.aliyun.iotx.framework.ui.driver.IoTDriver;
 import org.testng.annotations.Test;
-import com.codeborne.selenide.Configuration;
-import testInit.BaseTest;
+
 
 public class JavaUITest extends BaseTest {
+
     @Test
-    public void test(){
-        {$testCodes}
-        //java code ending
+    public void test() {
+
+        IoTDriver.startup();
+
+        Operation operation = new Operation() {
+            @Override
+            public void doSomeThing() {
+                {$testCodes}
+                //java code ending
+            }
+        };
+
+        {$openUrl}
+        
     }
 }
